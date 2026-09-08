7604da68adff67c612c2d5dc363079fc9bb74c01
bernabas-ab3 <bernabasabraham3@gmail.com>
2026-09-08 21:03:30 +0300

    Prefer IPv4 database connections

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  family: 4,
   connectionTimeoutMillis: 10000,
   idleTimeoutMillis: 30000,
   ssl: process.env.NODE_ENV === 'production'
